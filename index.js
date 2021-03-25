27f2f279a91a56b875f8233c2b61a933bad34863
AJ888 <ammarjirjees@gmail.com>
2021-03-24 19:27:01 -0500

    run first node

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-const express = require('express');
-const app = express()
-const port = 3100;
+const http = require('http');
 
-app.get('/', (req, res) => res.send('Welcome!'));
+const hostname = '127.0.0.1';
+const port = 3000;
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
